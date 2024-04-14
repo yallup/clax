2c87df5fb9a707f29d823d6a92be5c004e0551a0
David Yallup <david.yallup@gmail.com>
2024-04-14 21:12:47 +0100

    tweak some settings

--- clax/clax.py
+++ clax/clax.py
@@ -8,21 +8,26 @@ import optax
 from flax import linen as nn
 from jax import jit, tree_map
 from tqdm import tqdm
-
+from optax import tree_utils as otu
 from clax.network import DataLoader, Network, TrainState
 
 
 @dataclass
 class Trace:
+    """
+    Dataclass to store the training trace.
+    """
+
     iteration: int = field(default=0)
     losses: list[float] = field(default_factory=list)
+    lr: list[float] = field(default_factory=list)
 
 
 class Classifier(object):
-    network: nn.Module = Network
+    """Classifier class wrapping a basic jax multiclass classifier."""
 
     def __init__(self, n=2, **kwargs):
-        """Initialise the network.
+        """Initialise the Classifier.
 
         args:
             n: int: Number of classes.
@@ -44,19 +49,20 @@ class Classifier(object):
         # ).mean()
         return loss
 
-    def _train(self, samples, labels, **kwargs):
+    def _train(self, samples, labels, batches_per_epoch, **kwargs):
         """Internal wrapping of training loop."""
         self.trace = Trace()
         labels = jnp.array(labels, dtype=int)
         samples = jnp.array(samples, dtype=jnp.float32)
         batch_size = kwargs.get("batch_size", 1024)
-        data_size = samples.shape[0]
         epochs = kwargs.get("epochs", 10)
-        epochs *= data_size // batch_size
+        epochs *= batches_per_epoch
 
         @jit
         def update_step(state, samples, labels):
-            val, grads = jax.value_and_grad(self.loss)(state.params, samples, labels)
+            val, grads = jax.value_and_grad(self.loss)(
+                state.params, samples, labels
+            )
             state = state.apply_gradients(grads=grads, value=val)
             return val, state
 
@@ -73,13 +79,16 @@ class Classifier(object):
             loss, self.state = update_step(self.state, batch, batch_label)
             losses.append(loss)
             # self.state.losses.append(loss)
-            if (k + 1) % 100 == 0:
-                ma = jnp.mean(jnp.array(losses[-100:]))
+            if (k + 1) % 50 == 0:
+                ma = jnp.mean(jnp.array(losses[-50:]))
                 self.trace.losses.append(ma)
                 tepochs.set_postfix(loss=ma)
                 self.trace.iteration += 1
+                lr_scale = otu.tree_get(self.state, "scale")
+                self.trace.lr.append(lr_scale)
 
     def _init_state(self, **kwargs):
+        """Initialise the training state and setup the optimizer."""
         dummy_x = jnp.zeros((1, self.ndims))
         _params = self.network.init(self.rng, dummy_x)
         lr = kwargs.get("lr", 1e-3)
@@ -87,11 +96,14 @@ class Classifier(object):
         transition_steps = kwargs.get("transition_steps", 100)
         optimizer = optax.chain(
             optax.clip_by_global_norm(1.0),
-            optax.adamw(lr),
+            optax.adamw(
+                optax.cosine_decay_schedule(lr * 5, transition_steps, alpha=lr)
+            ),
             optax.contrib.reduce_on_plateau(
                 factor=0.5,
-                patience=transition_steps,
-                cooldown=transition_steps // 2,
+                patience=transition_steps // 10,  # 10
+                # cooldown=transition_steps // 10,  # 10
+                accumulation_size=transition_steps,
             ),
         )
 
@@ -102,7 +114,7 @@ class Classifier(object):
         )
 
     def fit(self, samples, labels, **kwargs):
-        """Calibrate the model on the provided data.
+        """Fit the classifier on provided samples.
 
         Args:
             samples (np.ndarray): Samples to train on.
@@ -113,12 +125,17 @@ class Classifier(object):
             batch_size (int): Size of the training batches. Defaults to 1024.
             epochs (int): Number of training epochs. Defaults to 10.
             lr (float): Learning rate. Defaults to 1e-3.
+            transition_steps (int): Number of steps to transition the learning rate.
+                                    Defaults to 100.
         """
         restart = kwargs.get("restart", False)
+        batch_size = kwargs.get("batch_size", 1024)
+        data_size = samples.shape[0]
+        batches_per_epoch = data_size // batch_size
         self.ndims = samples.shape[-1]
         if (not self.state) | restart:
             self._init_state(**kwargs)
-        self._train(samples, labels, **kwargs)
+        self._train(samples, labels, batches_per_epoch, **kwargs)
         self._predict_weight = lambda x: self.state.apply_fn(
             {
                 "params": self.state.params,
