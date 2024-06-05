14ab48df128e7ee790d71ac6c1195fd0964e921f
yallup <david.yallup@gmail.com>
2024-06-05 16:00:24 +0100

    readd batch norm

--- clax/clax.py
+++ clax/clax.py
@@ -28,7 +28,7 @@ class Trace:
 class Classifier(object):
     """Classifier class wrapping a basic jax multiclass classifier."""
 
-    def __init__(self, n=2, **kwargs):
+    def __init__(self, n=1, **kwargs):
         """Initialise the Classifier.
 
         args:
@@ -40,14 +40,19 @@ class Classifier(object):
         self.network = Network(n_out=n)
         self.state = None
 
-    def loss(self, params, batch, labels):
+    def loss(self, params, batch_stats, batch, labels):
         """Loss function for training the classifier."""
-        output = self.state.apply_fn({"params": params}, batch)
+        output, updates = self.state.apply_fn(
+            {"params": params, "batch_stats": batch_stats},
+            batch,
+            train=True,
+            mutable=["batch_stats"],
+        )
         # loss = optax.softmax_cross_entropy_with_integer_labels(
         #     output.squeeze(), labels
         # ).mean()
         loss = optax.sigmoid_binary_cross_entropy(output.squeeze(), labels).mean()
-        return loss
+        return loss, updates
 
     def _train(self, samples, labels, batches_per_epoch, **kwargs):
         """Internal wrapping of training loop."""
@@ -60,8 +65,11 @@ class Classifier(object):
 
         @jit
         def update_step(state, samples, labels):
-            val, grads = jax.value_and_grad(self.loss)(state.params, samples, labels)
+            (val, updates), grads = jax.value_and_grad(self.loss, has_aux=True)(
+                state.params, state.batch_stats, samples, labels
+            )
             state = state.apply_gradients(grads=grads)  # , scale_value=val)
+            state = state.replace(batch_stats=updates["batch_stats"])
             return val, state
 
         train_size = samples.shape[0]
@@ -82,15 +90,16 @@ class Classifier(object):
                 self.trace.losses.append(ma)
                 tepochs.set_postfix(loss=ma)
                 self.trace.iteration += 1
-                lr_scale = otu.tree_get(self.state, "scale")
-                self.trace.lr.append(lr_scale)
+                # lr_scale = otu.tree_get(self.state, "scale")
+                # self.trace.lr.append(lr_scale)
 
     def _init_state(self, **kwargs):
         """Initialise the training state and setup the optimizer."""
         dummy_x = jnp.zeros((1, self.ndims))
-        _params = self.network.init(self.rng, dummy_x)
+        _params = self.network.init(self.rng, dummy_x, train=False)
         lr = kwargs.get("lr", 1e-2)
         params = _params["params"]
+        batch_stats = _params["batch_stats"]
         transition_steps = kwargs.get("transition_steps", 1000)
         self.schedule = optax.warmup_cosine_decay_schedule(
             init_value=0.0,
@@ -104,13 +113,15 @@ class Classifier(object):
             # optax.clip_by_global_norm(1.0),
             optax.adaptive_grad_clip(0.01),
             # optax.adam(lr),
-            optax.adamw(self.schedule),
+            # optax.adamw(self.schedule),
+            optax.adamw(lr),
         )
 
-        self.state = train_state.TrainState.create(
-            # self.state = TrainState.create(
+        # self.state = train_state.TrainState.create(
+        self.state = TrainState.create(
             apply_fn=self.network.apply,
             params=params,
+            batch_stats=batch_stats,
             tx=optimizer,
         )
 
@@ -140,8 +151,10 @@ class Classifier(object):
         self._predict_weight = lambda x: self.state.apply_fn(
             {
                 "params": self.state.params,
+                "batch_stats": self.state.batch_stats,
             },
             x,
+            train=False,
         )
 
     def predict(self, samples, log=True):
@@ -155,6 +168,12 @@ class Classifier(object):
             return self._predict_weight(samples)
         else:
             return nn.softmax(self._predict_weight(samples))
+
+    def log_score(self, samples):
+        logits = self._predict_weight(samples)
+        log_p = jax.nn.log_sigmoid(logits)
+        log_not_p = jax.nn.log_sigmoid(-logits)
+        return log_p - (1 - log_not_p)
 
 
 class Regressor(Classifier):
--- clax/network.py
+++ clax/network.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import numpy as np
 import optax
 from flax import linen as nn
@@ -18,69 +20,69 @@ class DataLoader(object):
 
 
 class TrainState(train_state.TrainState):
-    # batch_stats: Any
-    scale_value: float = 1.0
+    batch_stats: Any
+    # scale_value: float = 1.0
 
-    def apply_gradients(self, *, grads, **kwargs):
-        """Updates `step`, `params`, `opt_state` and `**kwargs` in return value.
+    # def apply_gradients(self, *, grads, **kwargs):
+    #     """Updates `step`, `params`, `opt_state` and `**kwargs` in return value.
 
-        Note that internally this function calls `.tx.update()` followed by a call
-        to `optax.apply_updates()` to update `params` and `opt_state`.
+    #     Note that internally this function calls `.tx.update()` followed by a call
+    #     to `optax.apply_updates()` to update `params` and `opt_state`.
 
-        Args:
-        grads: Gradients that have the same pytree structure as `.params`.
-        **kwargs: Additional dataclass attributes that should be `.replace()`-ed.
+    #     Args:
+    #     grads: Gradients that have the same pytree structure as `.params`.
+    #     **kwargs: Additional dataclass attributes that should be `.replace()`-ed.
 
-        Returns:
-        An updated instance of `self` with `step` incremented by one, `params`
-        and `opt_state` updated by applying `grads`, and additional attributes
-        replaced as specified by `kwargs`.
-        """
-        scale_value = kwargs.get("scale_value", 1.0)
-        if OVERWRITE_WITH_GRADIENT in grads:
-            grads_with_opt = grads["params"]
-            params_with_opt = self.params["params"]
-        else:
-            grads_with_opt = grads
-            params_with_opt = self.params
+    #     Returns:
+    #     An updated instance of `self` with `step` incremented by one, `params`
+    #     and `opt_state` updated by applying `grads`, and additional attributes
+    #     replaced as specified by `kwargs`.
+    #     """
+    #     scale_value = kwargs.get("scale_value", 1.0)
+    #     if OVERWRITE_WITH_GRADIENT in grads:
+    #         grads_with_opt = grads["params"]
+    #         params_with_opt = self.params["params"]
+    #     else:
+    #         grads_with_opt = grads
+    #         params_with_opt = self.params
 
-        updates, new_opt_state = self.tx.update(
-            grads_with_opt, self.opt_state, params_with_opt, value=scale_value
-        )
-        new_params_with_opt = optax.apply_updates(params_with_opt, updates)
+    #     updates, new_opt_state = self.tx.update(
+    #         grads_with_opt, self.opt_state, params_with_opt, value=scale_value
+    #     )
+    #     new_params_with_opt = optax.apply_updates(params_with_opt, updates)
 
-        # As implied by the OWG name, the gradients are used directly to update the
-        # parameters.
-        if OVERWRITE_WITH_GRADIENT in grads:
-            new_params = {
-                "params": new_params_with_opt,
-                OVERWRITE_WITH_GRADIENT: grads[OVERWRITE_WITH_GRADIENT],
-            }
-        else:
-            new_params = new_params_with_opt
-        return self.replace(
-            step=self.step + 1,
-            params=new_params,
-            opt_state=new_opt_state,
-            **kwargs,
-        )
+    #     # As implied by the OWG name, the gradients are used directly to update the
+    #     # parameters.
+    #     if OVERWRITE_WITH_GRADIENT in grads:
+    #         new_params = {
+    #             "params": new_params_with_opt,
+    #             OVERWRITE_WITH_GRADIENT: grads[OVERWRITE_WITH_GRADIENT],
+    #         }
+    #     else:
+    #         new_params = new_params_with_opt
+    #     return self.replace(
+    #         step=self.step + 1,
+    #         params=new_params,
+    #         opt_state=new_opt_state,
+    #         **kwargs,
+    #     )
 
-    @classmethod
-    def create(cls, *, apply_fn, params, tx, **kwargs):
-        """Creates a new instance with `step=0` and initialized `opt_state`."""
-        # We exclude OWG params when present because they do not need opt states.
-        params_with_opt = (
-            params["params"] if OVERWRITE_WITH_GRADIENT in params else params
-        )
-        opt_state = tx.init(params_with_opt)
-        return cls(
-            step=0,
-            apply_fn=apply_fn,
-            params=params,
-            tx=tx,
-            opt_state=opt_state,
-            **kwargs,
-        )
+    # @classmethod
+    # def create(cls, *, apply_fn, params, tx, **kwargs):
+    #     """Creates a new instance with `step=0` and initialized `opt_state`."""
+    #     # We exclude OWG params when present because they do not need opt states.
+    #     params_with_opt = (
+    #         params["params"] if OVERWRITE_WITH_GRADIENT in params else params
+    #     )
+    #     opt_state = tx.init(params_with_opt)
+    #     return cls(
+    #         step=0,
+    #         apply_fn=apply_fn,
+    #         params=params,
+    #         tx=tx,
+    #         opt_state=opt_state,
+    #         **kwargs,
+    #     )
 
 
 class Network(nn.Module):
@@ -93,10 +95,37 @@ class Network(nn.Module):
     # act = nn.silu
 
     @nn.compact
-    def __call__(self, x):
+    def __call__(self, x, train: bool):
+        x = nn.Dense(self.n_initial)(x)
+        x = nn.BatchNorm(use_running_average=not train)(x)
+        x = nn.silu(x)
+        for i in range(self.n_layers):
+            x = nn.Dense(self.n_hidden)(x)
+            x = nn.BatchNorm(use_running_average=not train)(x)
+            x = nn.silu(x)
+        x = nn.Dense(self.n_out)(x)
+        return x
+
+
+from jax import numpy as jnp
+
+
+class ConditionalNetwork(nn.Module):
+    """A simple MLP classifier."""
+
+    n_initial: int = 256
+    n_hidden: int = 64
+    n_layers: int = 3
+    n_out: int = 1
+    # act = nn.silu
+
+    @nn.compact
+    def __call__(self, x, y):
+        x = nn.concatenate([x, y])
         x = nn.Dense(self.n_initial)(x)
         x = nn.silu(x)
         for i in range(self.n_layers):
+            x = jnp.concatenate([x, y])
             x = nn.Dense(self.n_hidden)(x)
             x = nn.silu(x)
         x = nn.Dense(self.n_out)(x)
