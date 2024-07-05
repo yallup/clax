83a2515abce3e64b16948e0300c74fb1beb09937
yallup <david.yallup@gmail.com>
2024-07-05 17:25:11 +0100

    fix tests to just use jax

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,8 +1,10 @@
-import numpy as np
+import jax.numpy as jnp
 import pytest
-from jax import nn
+from jax import nn, random
 
 from clax import Classifier, ClassifierSamples
+
+rng = random.PRNGKey(0)
 
 # @pytest.mark.parametrize("n_classes", [2, 10])
 # class TestClassifier:
@@ -19,18 +21,18 @@ from clax import Classifier, ClassifierSamples
 @pytest.mark.parametrize("n_classes", [1, 2, 10])
 def test_classifier(n_classes):
     classifier = Classifier(n_classes)
-    data_x = np.random.rand(100, 10)
-    data_y = np.random.randint(0, n_classes, 100)
+    data_x = random.uniform(rng, (100, 10))
+    data_y = random.randint(rng, (100,), 0, n_classes)
     classifier.fit(data_x, data_y)
     y = classifier.predict(data_x)
     assert y.shape == (100, n_classes)
-    assert np.isclose(nn.softmax(y).sum(axis=-1), 1).all()
+    assert jnp.isclose(nn.softmax(y).sum(axis=-1), 1).all()
 
 
 def test_conditional_classifier():
     classifier = ClassifierSamples()
-    data_x = np.random.rand(100, 10)
-    data_y = np.random.rand(100, 10)
+    data_x = random.normal(rng, (100, 10))
+    data_y = random.normal(rng, (100, 10))
     classifier.fit(data_x, data_y)
     y = classifier.predict(data_x)
     assert y.shape == (100, 1)
