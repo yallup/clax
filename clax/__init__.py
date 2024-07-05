18f4ab25b70cff0e34b129f3949150681eb3d8c3
yallup <david.yallup@gmail.com>
2024-07-05 11:24:47 +0100

    flake

--- clax/__init__.py
+++ clax/__init__.py
@@ -1,6 +1,6 @@
 """clax: simple jax classifiers."""
 
 from clax._version import __version__  # noqa: F401
-from clax.clax import Classifier, ClassifierSamples, Regressor
+from clax.clax import Classifier, ClassifierSamples, Regressor  # noqa: F401
 
-__all__ = ["Classifier", "ClassifierSamples", "Regressor"]
+# __all__ = ["Classifier", "ClassifierSamples", "Regressor"]
